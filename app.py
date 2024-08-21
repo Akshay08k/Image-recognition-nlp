286531518603463c8d3b18c9ee0fe81a153cf7f8
akshay <akshaykomade012345@gmail.com>
2024-08-21 17:40:27 +0530

    Added documentation

--- app.py
+++ app.py
@@ -1,42 +1,31 @@
-# from flask import Flask, request, jsonify, render_template
-# from werkzeug.utils import secure_filename
-# import os
-# from image_recognition import classify_image, generate_description
-
-# app = Flask(__name__)
-# UPLOAD_FOLDER = 'uploads/'
-# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-# @app.route('/')
-# def home():
-#     return render_template('index.html')
-
-# @app.route('/upload', methods=['POST'])
-# def upload_image():
-#     if 'file' not in request.files:
-#         return "No file part"
-#     file = request.files['file']
-#     if file.filename == '':
-#         return "No selected file"
-#     if file:
-#         filename = secure_filename(file.filename)
-#         file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-#         file.save(file_path)
-
-#         predictions = classify_image(file_path)
-#         description = generate_description(predictions)
-#         return jsonify({"description": description})
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
-
-from flask import Flask
+from flask import Flask, request, jsonify, render_template
+from werkzeug.utils import secure_filename
+import os
+from image_recognition import classify_image, generate_description
 
 app = Flask(__name__)
+UPLOAD_FOLDER = 'uploads/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home():
+    return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+@app.route('/upload', methods=['POST'])
+def upload_image():
+    if 'file' not in request.files:
+        return "No file part"
+    file = request.files['file']
+    if file.filename == '':
+        return "No selected file"
+    if file:
+        filename = secure_filename(file.filename)
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(file_path)
+
+        predictions = classify_image(file_path)
+        description = generate_description(predictions)
+        return jsonify({"description": description})
+
+if __name__ == "__main__":
+    app.run(debug=True)
